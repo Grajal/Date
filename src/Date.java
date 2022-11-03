78b7bcfbcd474dbcb01f7bba86ba0e641b87a9f5
Jorge Grajal <jgrajh00@estudiantes.unileon.es>
2022-11-03 12:32:33 +0100

    getRandomDateInsideYear() realizado =

--- src/Date.java
+++ src/Date.java
@@ -89,7 +89,53 @@ public class Date{
     }
 
     public String getNameMonth(){
+        switch(this.getMonth()){
+            case 1:
+                return "January";
+            case 2:     
+                return "February";
+            case 3: 
+                return "March";
+            case 4: 
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7: 
+                return "July";
+            case 8: 
+                return "August";
+            case 9: 
+                return "September";
+            case 10: 
+                return "October";
+            case 11: 
+                return "November";
+            case 12: 
+                return "December";
+      
+        }
+    }
 
+    public boolean validDay(){
+        boolean isValid=true;
+    
+        switch(this.getMonth()){ 
+            case 2:
+                if(this.getDay()>28) isValid=false;
+                break;
+            case 4:     
+            case 6: 
+            case 4: 
+            case 6:
+            case 9:
+            case 11: 
+                if(this.getDay()>30) isValid=false;
+                break;
+
+            return isValid;
+        }
     }
 
 }
